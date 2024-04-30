faae0a6928e14250dabec83b504caea980750f5d
user <user@DESKTOP-O5OB743>
2024-04-30 14:24:55 +0900

    시작

---
+++ datingproject/src/test/java/com/example/datingproject/DatingprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
